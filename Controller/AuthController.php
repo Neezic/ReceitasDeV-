f08e1ee5379de5b293cb0a9d5896fdb309958e21
Neezic <joaolucasdealmeida1@gmail.com>
2025-06-02 11:08:03 -0300

    Model de usuario e controlers de usuario atualizados

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -1,20 +1,27 @@
 <?php
-//Sistema de Login
+session_start();
+require_once '../Config/banco.php';
+require_once '../Model/Usuario.php';
 
-if ($_POST && $_POST['acao'] == 'login') {
+if ($_POST && isset($_POST['acao']) && $_POST['acao'] == 'login') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
     
-    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = ?");
-    $stmt->execute([$email]);
-    $usuario = $stmt->fetch();
-    
-    if ($usuario && password_verify($senha, $usuario['senha'])) {
-        $_SESSION['usuario'] = $usuario;
+    $usuarioModel = new Usuario($pdo);
+    $usuarioEncontrado = $usuarioModel -> buscarPorEmail($email);
+
+    if ($usuarioEcontrado && password_verify($senha,$usuarioEncontrado['senha'])){
+        unset($usuarioEncontrado['senha']);
+        $_SESSION['usuario'] = $usuarioEcontrado;
         header('Location: ?pagina=home');
+        exit;
     } else {
-        $erro = "E-mail ou senha inválidos!";
+        $erro ="Email ou senha invalidos!";
+        include '../View/login.php';
+        exit;
     }
+    
+   
 }
 
 include 'view/login.php';
--- Model/Usuario.php
+++ Model/Usuario.php
@@ -1,3 +1,67 @@
 <?php
+    class Usuario{
+        private $pdo;
+        private $tabela = 'usuario';
 
+        public $id;
+        public $nome;
+        public $email;
+        public $senha;
+
+        public function __contruct(PDO $db){
+            $this -> pdo = $db;
+        }
+
+        public function criar(string $nome, string $email, string $senha) : bool {
+            $query = "INSERT INTO " . $this->tabela . " (nome, email, senha) VALUES (:nome, :email, :senha)";
+            
+            $stmt = $this -> pdo -> prepare($query);
+            $this -> nome = htmlspecialchars(strip_tags($nome));
+            $this -> email = htmlspecialchars(strip_tags($email));
+            $this -> senha = password_hash($senha, PASSWORD_DEFAULT);
+            
+            $stmt -> bindParam(':nome',$this->nome);
+            $stmt -> bindParam(':senha',$this->nome);
+            $stmt -> bindParam(':senha',$this->senha);  
+            
+            if ($stmt-> execute()){
+                return true;
+            }
+            return false;
+            
+        }
+
+        public function buscarPorId(int $id){
+            $query ="SELECT id, nome, email FROM " . $this->tabela . " WHERE id = :id LIMIT 1";
+
+            $stmt = $this -> pdo -> prepare($query);
+            $stmt-> bindParam(':id', $id);
+            $stmt -> execute();
+
+            if ($stmt-> rowCount() > 0 ){
+                $linha = $stmt-> fetch(PDO::FETCH_ASSOC);
+                return $linha;
+            }
+            return false;
+        }
+        public function buscarPorEmail(int $email){
+            $query ="SELECT id, nome, email FROM " . $this->tabela . " WHERE email = :email LIMIT 1";
+
+            $stmt = $this -> pdo -> prepare($query);
+            $stmt-> bindParam(':email', $email);
+            $stmt -> execute();
+
+            if ($stmt-> rowCount() > 0 ){
+                $linha = $stmt-> fetch(PDO::FETCH_ASSOC);
+                return $linha;
+            }
+            return false;
+        }
+        public function emailExiste(string $email) : bool{
+            $query = "SELECT id FROM " . $this->tabela . " WHERE email = :email LIMIT 1";
+            $stmt = $this -> pdo -> prepare($query);
+            $stmt-> bindParam(':email', $email);
+            return $stmt-> rowCount() > 0;
+        }
+    }
 ?>
